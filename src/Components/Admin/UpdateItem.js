08509379144fbf462602da2c64d2f148dedd4a6c
macktsawyer <macktsawyer@yahoo.com>
2022-04-20 20:07:48 -0400

    Working on front end of update item

---
+++ src/Components/Admin/UpdateItem.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+const UpdateItem = () => {
+  const [ itemToUpdate, setItemToUpdate ] = useState({})
+  return (
+    <div>UpdateItem</div>
+  )
+}
+
+export default UpdateItem
