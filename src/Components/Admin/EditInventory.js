641d0c161dc4229d72869c5aa9441944ab6f5a34
macktsawyer <macktsawyer@yahoo.com>
2022-07-13 22:05:04 -0400

    Fixing button styles

--- src/Components/Admin/EditInventory.js
+++ src/Components/Admin/EditInventory.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import Card from '@mui/material/Card';
 import { Image } from 'cloudinary-react';
